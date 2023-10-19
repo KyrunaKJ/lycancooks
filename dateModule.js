e3c4804e8e9133af929d0ba6216ec5662cf8e9a1
KyrunaKJ <kyrunakj@gmail.com>
2023-10-18 21:01:28 -0400

    Included interaction for most elements on the screen

---
+++ dateModule.js
@@ -0,0 +1,18 @@
+export function setCurrentDay() {// Create a new Date object to fetch the current date
+    const currentDate = new Date();
+
+    // Define an array to map day names
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    // Get the current day of the week (0-6)
+    const currentDay = currentDate.getDay();
+    const currentMonth = currentDate.getMonth();
+    const currentDayOfMonth = currentDate.getDate();
+
+    // Use the current day to fetch the corresponding day name from the array
+    const currentDayName = daysOfWeek[currentDay];
+    const currentMonthName = months[currentMonth];
+
+    return `${currentDayName}, ${currentMonthName} ${currentDayOfMonth}`;
+}
---
+++ main.js
@@ -0,0 +1,117 @@
+import { setCurrentDay } from './dateModule.js'
+
+// Get a reference to the element with the ID "current-day"
+const currentDayElement = document.getElementById("current-day");
+currentDayElement.textContent = setCurrentDay();
+
+let recipes = [];
+const recipeList = document.querySelector(".recipe-list ul");
+const tabButtonsContainers = document.querySelectorAll(".tab-buttons");
+const groceryItemList = document.querySelector(".grocery-list ul");
+const recipeInstructionList = document.querySelector(".recipe-instructions ul");
+const showAllButton = document.getElementById("show-all-groceries");
+const instructionsHeader = document.getElementById("instructions");
+const groceriesHeader = document.getElementById("groceries");
+
+// Function to clear the grocery list
+function clearGroceryList() {
+    while (groceryItemList.firstChild) {
+        groceryItemList.removeChild(groceryItemList.firstChild);        
+    }
+}
+
+function replaceGroceryList(event) {
+    const clickedButton = event.target;
+    clearGroceryList();
+    
+    if (clickedButton === showAllButton) {
+        recipes.forEach((recipe) => {
+                recipe.groceryList.forEach((grocery) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = `${grocery.amount} ${grocery.unit} of ${grocery.item}`;
+                groceryItemList.appendChild(listItem);
+            });
+        });
+        groceriesHeader.textContent = "Groceries for Today's Recipes"
+        instructionsHeader.textContent = "";
+    } else {
+        const recipeId = clickedButton.getAttribute("data-recipe");
+        const recipe = recipes[recipeId-1];
+        recipe.groceryList.forEach((grocery) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${grocery.amount} ${grocery.unit} of ${grocery.item}`;
+            groceryItemList.appendChild(listItem);
+        });
+        groceriesHeader.textContent = `Groceries for Today's Recipes #${recipeId}`;
+        instructionsHeader.textContent = `Recipe ${recipeId} Instructions`;
+    }
+}
+
+function clearRecipeList() {
+    while (recipeInstructionList.firstChild) {
+        recipeInstructionList.removeChild(recipeInstructionList.firstChild);
+    }
+}
+
+function replaceRecipeList(event) {
+    const clickedButton = event.target;
+
+    const recipeId = clickedButton.getAttribute("data-recipe");
+    const recipe = recipes[recipeId-1];
+
+    clearRecipeList();
+
+    recipe.instructions.forEach((inst) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = inst;
+        recipeInstructionList.appendChild(listItem);
+    });
+}
+
+showAllButton.addEventListener('click', replaceGroceryList);
+showAllButton.addEventListener('click', replaceRecipeList);
+
+fetch("recipes.json")
+    .then((response) => response.text())
+    .then((jsontext) => {
+        recipes = JSON.parse(jsontext);
+        clearGroceryList();
+        clearRecipeList();
+
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        recipes.forEach((recipe, index) => {
+            const listItem = document.createElement("li");
+            const recipeButton = document.createElement("button");
+            const groceryListItem = document.createElement("li");
+        
+            listItem.textContent = recipe.name;
+            listItem.setAttribute("data-recipe", (index + 1));
+        
+            recipeButton.textContent = "Recipe " + (index + 1);
+            recipeButton.classList.add("tab-button");
+            recipeButton.setAttribute("data-recipe", (index + 1));
+
+            recipe.groceryList.forEach((gli) => {  
+                const groceryListItemClone = groceryListItem.cloneNode(true);
+                groceryListItemClone.textContent = `${gli.amount} ${gli.unit} of ${gli.item}`;
+                groceryItemList.appendChild(groceryListItemClone);
+            });
+        
+            recipeList.appendChild(listItem);
+            tabButtonsContainers.forEach((container) => {
+                const buttonClone = recipeButton.cloneNode(true);
+                buttonClone.addEventListener('click', replaceGroceryList);
+                buttonClone.addEventListener('click', replaceRecipeList);
+                container.appendChild(buttonClone);
+            });
+
+            if (index === 0) {
+                recipe.instructions.forEach((inst) => {                    
+                    const recipeListItem = document.createElement("li");
+                    recipeListItem.textContent = inst;
+                    recipeInstructionList.appendChild(recipeListItem);
+                });
+            }
+        });
+});
